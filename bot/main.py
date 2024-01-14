7e8d2677f6296dd8e83c397d439b323afb2160a9
Riho0511 <riho.pinehill@gmail.com>
2024-01-14 18:38:41 +0900

    いきたいスタンプが押されたメッセージを取得できるようにした

--- bot/main.py
+++ bot/main.py
@@ -8,4 +8,17 @@ client = discord.Client(intents=intents)
 async def on_ready():
   print("Ready!")
 
+# いきたいスタンプが押された時にお誘いチャンネルに自動でスレッドを立てる
+target_reaction = "ikitai"
+@client.event
+async def on_raw_reaction_add(payload):
+  channel = client.get_channel(payload.channel_id)
+  message = await channel.fetch_message(payload.message_id)
+  reaction = payload.emoji.name
+
+  if reaction != target_reaction:
+    return
+
+  print(message.system_content)
+
 client.run(config.DISCORD_TOKEN)
