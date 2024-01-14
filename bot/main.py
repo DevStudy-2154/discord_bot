c00171875742a992f9571b825a56079523aa7051
Riho0511 <riho.pinehill@gmail.com>
2024-01-14 18:36:04 +0900

    不要なので削除

--- bot/main.py
+++ bot/main.py
@@ -8,12 +8,4 @@ client = discord.Client(intents=intents)
 async def on_ready():
   print("Ready!")
 
-# 動作確認(おうむ返し)
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  await message.channel.send("Hello World!")
-
 client.run(config.DISCORD_TOKEN)
