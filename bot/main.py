a7fad16b722154fb1824f7a2d759bb5f82e3e541
Riho0511 <riho.pinehill@gmail.com>
2024-01-15 20:57:40 +0900

    変数の定義位置をメソッド内に移動

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import discord
+import random, string
 import settings.config as config
 
 intents = discord.Intents.default()
@@ -9,17 +10,27 @@ async def on_ready():
   print("Ready!")
 
 # いきたいスタンプが押されたらお誘いチャンネルに自動でスレッドを立てる
-target_reaction = "ikitai"
 @client.event
 async def on_raw_reaction_add(payload):
-  channel = client.get_channel(payload.channel_id)
+  target_reaction = "ikitai"
+  invitation_channel = client.get_channel(config.INVITATION_CHANNEL_ID)
+  target_channel = client.get_channel(payload.channel_id)
   reaction = payload.emoji.name
 
   if reaction != target_reaction:
     return
 
-  message_link = f'https://discord.com/channels/{config.SERVER_ID}/{channel.id}/{payload.message_id}'
+  """
+  初期生成スレッド情報
+  スレッド名：ランダム文字列
+  初期メッセージ：スタンプが押されたメッセージリンク
+  """
+  message_link = f'https://discord.com/channels/{config.SERVER_ID}/{target_channel.id}/{payload.message_id}'
+  thread_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+  thread = await invitation_channel.create_thread(name=thread_name,type=discord.ChannelType.public_thread)
+  thread_link = thread.mention
 
-  await channel.send(message_link)
+  await thread.send(message_link)
+  await target_channel.send(thread_link)
 
 client.run(config.DISCORD_TOKEN)
